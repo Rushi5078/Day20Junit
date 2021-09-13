c78771d5e5ae9e64952545d5df59a97d0c27b2cd
Rushi5078 <88040056+Rushi5078@users.noreply.github.com>
2021-09-13 19:10:22 +0530

    Add: initial commit

---
+++ PasswordValidationRule4.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package RegexProblem;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author RUSHIKESH BHOSLE
+ *
+ */
+
+public class PasswordValidationRule4 
+{
+	/**
+	 * @purpose:Checking enter valid password rules4 at list 1 Special Symbol
+	 * @return: boolean true or false
+	 */
+
+	public static boolean checkValidPassword(String password) {
+		boolean ispassword;// check the validation
+		String passwordRegex = "^[A-Z]{1}[a-z]{7,}[@]{1}[0-9]{1,}$";
+		Pattern pattern = Pattern.compile(passwordRegex);
+		if (password == null) {
+			ispassword = false;
+		}
+		Matcher matcher = pattern.matcher(password);
+		ispassword = matcher.matches();
+		// print the output
+		System.out.println(ispassword);
+		return  matcher.matches();
+	}
+
+	public static void main(String[] args) {
+		// Calling the method
+		checkValidPassword("Rushikesh@123");
+	}
+
+}
